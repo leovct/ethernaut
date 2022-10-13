4d7b8cf6e1e15488da52d955b0890ff423b310b0
leovct <l009.vincent@gmail.com>
2022-10-13 11:18:26 +0200

    chore: fix indentation

--- src/Ethernaut.sol
+++ src/Ethernaut.sol
@@ -8,79 +8,71 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "ethernaut/contracts/levels/base/Level.sol";
 
 contract Ethernaut is Ownable {
-    // ----------------------------------
-    // Owner interaction
-    // ----------------------------------
+	// ----------------------------------
+	// Owner interaction
+	// ----------------------------------
 
-    mapping(address => bool) private registeredLevels;
+	mapping(address => bool) private registeredLevels;
 
-    // Only registered levels will be allowed to generate and validate level instances.
-    function registerLevel(Level _level) public onlyOwner {
-        registeredLevels[address(_level)] = true;
-    }
+	// Only registered levels will be allowed to generate and validate level instances.
+	function registerLevel(Level _level) public onlyOwner {
+		registeredLevels[address(_level)] = true;
+	}
 
-    // ----------------------------------
-    // Get/submit level instances
-    // ----------------------------------
+	// ----------------------------------
+	// Get/submit level instances
+	// ----------------------------------
 
-    struct EmittedInstanceData {
-        address player;
-        Level level;
-        bool completed;
-    }
+	struct EmittedInstanceData {
+		address player;
+		Level level;
+		bool completed;
+	}
 
-    mapping(address => EmittedInstanceData) private emittedInstances;
+	mapping(address => EmittedInstanceData) private emittedInstances;
 
-    event LevelInstanceCreatedLog(address indexed player, address instance);
-    event LevelCompletedLog(address indexed player, Level level);
+	event LevelInstanceCreatedLog(address indexed player, address instance);
+	event LevelCompletedLog(address indexed player, Level level);
 
-    function createLevelInstance(Level _level)
-        public
-        payable
-        returns (address)
-    {
-        // Ensure level is registered.
-        // solhint-disable-next-line reason-string
-        require(registeredLevels[address(_level)]);
+	function createLevelInstance(Level _level) public payable returns (address) {
+		// Ensure level is registered.
+		// solhint-disable-next-line reason-string
+		require(registeredLevels[address(_level)]);
 
-        // Get level factory to create an instance.
-        address instance = _level.createInstance{value: msg.value}(msg.sender);
+		// Get level factory to create an instance.
+		address instance = _level.createInstance{value: msg.value}(msg.sender);
 
-        // Store emitted instance relationship with player and level.
-        emittedInstances[instance] = EmittedInstanceData(
-            msg.sender,
-            _level,
-            false
-        );
+		// Store emitted instance relationship with player and level.
+		emittedInstances[instance] = EmittedInstanceData(msg.sender, _level, false);
 
-        // Retrieve created instance via logs.
-        emit LevelInstanceCreatedLog(msg.sender, instance);
+		// Retrieve created instance via logs.
+		emit LevelInstanceCreatedLog(msg.sender, instance);
 
-        return instance; // PATCH: return data because it's not possible to read emitted events via Solidity
-    }
+		// PATCH: return data because it's not possible to read emitted events via Solidity
+		return instance;
+	}
 
-    function submitLevelInstance(address payable _instance)
-        public
-        returns (bool)
-    {
-        // Get player and level.
-        EmittedInstanceData storage data = emittedInstances[_instance];
-        // solhint-disable-next-line reason-string
-        require(data.player == msg.sender); // instance was emitted for this player
-        // solhint-disable-next-line reason-string
-        require(data.completed == false); // not already submitted
+	function submitLevelInstance(address payable _instance) public returns (bool) {
+		// Get player and level.
+		EmittedInstanceData storage data = emittedInstances[_instance];
+		// solhint-disable-next-line reason-string
+		require(data.player == msg.sender); // instance was emitted for this player
+		// solhint-disable-next-line reason-string
+		require(data.completed == false); // not already submitted
 
-        // Have the level check the instance.
-        if (data.level.validateInstance(_instance, msg.sender)) {
-            // Register instance as completed.
-            data.completed = true;
+		// Have the level check the instance.
+		if (data.level.validateInstance(_instance, msg.sender)) {
+			// Register instance as completed.
+			data.completed = true;
 
-            // Notify success via logs.
-            emit LevelCompletedLog(msg.sender, data.level);
+			// Notify success via logs.
+			emit LevelCompletedLog(msg.sender, data.level);
 
-            return true; // PATCH: return data because it's not possible to read emitted events
-        }
+			// PATCH: return data because it's not possible to read emitted events
+			return true;
+		}
 
-        return false; // PATCH: return data because it's not possible to read emitted events
-    }
+		// PATCH: return data because it's not possible to read emitted events
+		return false;
+	}
 }
